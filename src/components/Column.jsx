69ae1a3fd561e594cb461a70facffee5f1782062
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-05-08 21:54:53 +0100

    create and style modal to add new tasks

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -2,8 +2,12 @@ import { useStore } from '../store';
 import { shallow } from 'zustand/shallow';
 import './Column.css';
 import Task from './Task';
+import { useState } from 'react';
 
 export default function Column({ state }) {
+    const [text, setText] = useState('');
+    const [open, setOpen] = useState(false);
+
     const tasks = useStore(store => 
         store.tasks.filter(task => task.state === state),
         shallow
@@ -15,17 +19,27 @@ export default function Column({ state }) {
         <div className="column">
             <div className='titleWrapper'>
                 <p>{state}</p>
-                <button onClick={() => {
-                    console.log('addTask');
-                    addTask('test' + state, state);
-                    }}
-                >
-                    Add
-                </button>
+                <button onClick={() => setOpen(true)}>Add</button>
             </div>
             {tasks.map(task => (
                 <Task title={task.title} key={task.title} />
             ))}
+            {open && (
+                <div className='Modal'>
+                    <div className='modalContent'>
+                        <input onChange={e => setText(e.target.value)} value={text} />
+                        <button 
+                            onClick={() => {
+                                addTask(text, state);
+                                setText('');
+                                setOpen(false);
+                            }}
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
