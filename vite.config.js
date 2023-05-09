c5a80c5ff801b3ce7deea71bbcd116afa0fe687e
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-05-09 22:13:12 +0100

    deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/kanban_zustand",
   plugins: [react()],
   test: {
     globals: true,
